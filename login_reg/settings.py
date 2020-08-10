0247088ad9881fd00aa49e597c49baad0c3369df
XuMou <2745254260@qq.com>
2020-08-10 18:42:49 +0800

    配置celery配置项

--- login_reg/settings.py
+++ login_reg/settings.py
@@ -154,7 +154,7 @@ EMAIL_USE_SSL = True    #是否使用SSL加密，qq企业邮箱要求使用
 EMAIL_HOST = 'smtp.qq.com'   #发送邮件的邮箱 的 SMTP服务器，这里用了qq邮箱
 EMAIL_PORT=465  # qq邮箱的发送邮件的端口号
 EMAIL_HOST_USER='2745254260@qq.com' # 发送邮件的账号
-EMAIL_HOST_PASSWORD='pquyxxghsjildhdi' # 这里从qq邮箱申请的授权码
+EMAIL_HOST_PASSWORD='' # 这里从qq邮箱申请的授权码
 
 # 注册有效期天数
 CONFIRM_DAYS = 1
